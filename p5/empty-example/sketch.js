b984d6f513158a33bf3a8f89da0d03257a36f268
duong <nguye958@msu.edu>
2024-01-27 17:19:12 -0500

    collisions 2

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -40,65 +40,24 @@ function draw() {
     platforms[0].display();
     croc.display();
 
-
-    // Platform
-    platform.display();
-
     // Ground
     fill(0, 255, 0);
     rect(0, GROUND_LEVEL, width, height - GROUND_LEVEL);
 }
 
-class Platform{
-  constructor(){
-    this.width = random(50, 100);
-    this.height = 20;
-    this.x = width - this.width;
-    this.y = random(0, GROUND_LEVEL - this.height);
-  }
-  display(){
-    fill(151,87,43);
-    rect(this.x, this.y, this.width, this.height);
-  }
 class Platform {
-    constructor(x) {
-        this.x = x;
-        this.y = random(GROUND_LEVEL - 100, GROUND_LEVEL);
+    constructor() {
         this.width = random(50, 100);
-        this.height = 10;
+        this.height = 20;
+        this.x = width - this.width;
+        this.y = random(0, GROUND_LEVEL - this.height);
     }
     display() {
-        fill(0, 255, 0);
+        fill(151, 87, 43);
         rect(this.x - viewportX, this.y, this.width, this.height);
     }
 }
 
-class Player{
-  constructor(){
-    this.x = width / 2;
-    this.y = (height / 2) + 30;
-    this.vecY = 0;
-    this.onGround = false;
-    this.hp = BASE_HP;
-  }
-  display(){ 
-    fill(255, 255, 255)
-     rect(this.x, this.y, PLAYER_WIDTH, PLAYER_HEIGHT);
-  }
-  move(){
-    if (keyIsDown(LEFT_ARROW) || keyIsDown(LEFT_KEY)) {
-        this.x -= 5;
-    }
-    if (keyIsDown(RIGHT_ARROW) || keyIsDown(RIGHT_KEY)) {
-        this.x += 5;
-    }
-    if (keyIsDown(UP_ARROW) || keyIsDown(UP_KEY)) {
-        // Ensure the player is on the ground (no double jumping)
-        if (this.onGround) {
-            this.vecY = -10;
-            this.onGround = false;
-        }
-    }
 class Player {
     constructor() {
         this.x = width / 2;
@@ -112,12 +71,12 @@ class Player {
     }
     move() {
         if (keyIsDown(LEFT_ARROW) || keyIsDown(LEFT_KEY)) {
-            if (!this.leftCheck(platform)) {
+            if (!this.leftCheck()) {
                 this.x -= 5;
             }
         }
         if (keyIsDown(RIGHT_ARROW) || keyIsDown(RIGHT_KEY)) {
-            if (!this.rightCheck(platform)) {
+            if (!this.rightCheck()) {
                 this.x += 5;
             }
         }
@@ -133,65 +92,82 @@ class Player {
         viewportX = max(0, this.x - width / 2);
 
         // Gravity
-        if (this.vecY > 0 && this.groundCheck(platform)) {
+        if (this.vecY > 0 && this.groundCheck()) {
             this.vecY = 0;
-            this.onGround = true; 
-        } else if (!this.groundCheck(platform)) {
+            this.onGround = true;
+        } else if (!this.groundCheck()) {
             this.vecY += 0.5;
             this.onGround = false;
 
             // Check if player is hitting the top of the platform
-            if (this.topCheck(platform)) {
+            if (this.topCheck()) {
                 this.vecY = Math.abs(this.vecY * 0.4);
             }
         }
         this.y += this.vecY;
     }
-    rightCheck(platform) {
-        return this.y + PLAYER_HEIGHT > platform.y && this.y <= platform.y + platform.height && this.x + PLAYER_WIDTH >= platform.x && this.x + PLAYER_WIDTH + 10 <= platform.x + platform.width;
+    rightCheck() {
+        platforms.forEach(platform => {
+            if (this.y + PLAYER_HEIGHT > platform.y && this.y <= platform.y + platform.height && this.x + PLAYER_WIDTH >= platform.x && this.x + PLAYER_WIDTH + 10 <= platform.x + platform.width) {
+                return true;
+            }
+        });
+        return false;
     }
-    leftCheck(platform) {
-        return this.y + PLAYER_HEIGHT > platform.y && this.y <= platform.y + platform.height && this.x <= platform.x + platform.width && this.x >= platform.x;
+    leftCheck() {
+        platforms.forEach(platform => {
+            if (this.y + PLAYER_HEIGHT > platform.y && this.y <= platform.y + platform.height && this.x <= platform.x + platform.width && this.x >= platform.x) {
+                return true;
+            }
+        });
+        return false;
     }
-    groundCheck(platform) {
+    groundCheck() {
         if (this.y + PLAYER_HEIGHT >= GROUND_LEVEL) {
             this.y = GROUND_LEVEL - PLAYER_HEIGHT;
             return true;
         }
-        if (this.y + PLAYER_HEIGHT >= platform.y && this.y + PLAYER_HEIGHT <= platform.y + platform.height && this.x + PLAYER_WIDTH >= platform.x && this.x <= platform.x + platform.width) {
-            this.y = platform.y - PLAYER_HEIGHT;
-            return true;
-        }
+        platforms.forEach(platform => {
+            if (this.y + PLAYER_HEIGHT >= platform.y && this.y + PLAYER_HEIGHT <= platform.y + platform.height && this.x + PLAYER_WIDTH >= platform.x && this.x <= platform.x + platform.width) {
+                this.y = platform.y - PLAYER_HEIGHT;
+                return true;
+            }
+        });
         return false;
     }
-    topCheck(platform) {
-        return this.y >= platform.y && this.y <= platform.y + platform.height && this.x + PLAYER_WIDTH >= platform.x && this.x <= platform.x + platform.width;
+    topCheck() {
+        platforms.forEach(platform => {
+            if (this.y >= platform.y && this.y <= platform.y + platform.height && this.x + PLAYER_WIDTH >= platform.x && this.x <= platform.x + platform.width) {
+                return true;
+            }
+        });
+        return false;
     }
 }
 
-class Enemy{
-  constructor(){
-    this.x = 0;
-    this.y = 0;
-    this.damage;
-    this.hp;
-  }
+class Enemy {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.damage;
+        this.hp;
+    }
 }
 
-class Croc extends Enemy{
-  constructor(){
-    super();
-    this.x = width;
-    this.y = GROUND_LEVEL - 40;
-    this.damage = 1;
-    this.hp = 1;
-  }
-  display(){
-    image(imgCrocClosed, this.x-100, this.y, 100, 50);
-    noFill();
-    rect(this.x, this.y, 50, 50);
-  }
-  move(){
-    this.x -= 5;
-  }
+class Croc extends Enemy {
+    constructor() {
+        super();
+        this.x = width;
+        this.y = GROUND_LEVEL - 40;
+        this.damage = 1;
+        this.hp = 1;
+    }
+    display() {
+        image(imgCrocClosed, this.x - viewportX, this.y, 100, 50);
+        noFill();
+        rect(this.x - viewportX, this.y + 25, 100, 25);
+    }
+    move() {
+        this.x -= 5;
+    }
 }
