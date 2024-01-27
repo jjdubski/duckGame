43ba9647b59c2aebd479ae9549c5220601c664af
jjdubski <101855602+jjdubski@users.noreply.github.com>
2024-01-27 15:30:38 -0500

    update

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -3,10 +3,14 @@ let playerX, playerY;
 function setup() {
   createCanvas(400, 800);
   playerX = width / 2;
-  playerY = height / 2;
+  playerY = (height / 2) + 30;
 }
 
 function draw() {
-  background(220);
-  circle(playerX, playerY, 20)
+  background(173, 216, 230);
+  rect(playerX, playerY, 30,30)
+  playerX = playerX + 1;
+  if(playerX > width) {
+    playerX = 0;
+  }
 }
