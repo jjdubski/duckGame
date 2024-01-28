baa189938931d59eac7dc5c03b829510a77bc6d7
duong <nguye958@msu.edu>
2024-01-27 20:54:27 -0500

    player damage

--- p5/duckGame/player.js
+++ p5/duckGame/player.js
@@ -3,14 +3,30 @@ class Player {
     constructor() {
         this.x = width / 2;
         this.y = (height / 2) + 30;
-        this.vecY = 0;
+
+        this.velY = 0;
+        this.velX = 0;
+
         this.onGround = false;
         this.topCheck = false;
         this.leftCheck = false;
         this.rightCheck = false;
+
+        this.invincibility = 0;
     }
     display() {
-        fill(255, 255, 255);
+        // Invincibility frames
+        if (this.invincibility > 0) {
+            this.invincibility--;
+            
+        } 
+
+        // Flash the player if they are invincible
+        if (this.invincibility % 20 < 10) {
+            fill(255, 255, 255);
+        } else {
+            fill(255, 0, 0);
+        }
         rect(this.x - viewportX, this.y, PLAYER_WIDTH, PLAYER_HEIGHT);
     }
     move() {
@@ -21,34 +37,47 @@ class Player {
         viewportX = max(0, this.x - width / 2);
 
         // Gravity
-        if (this.vecY > 0 && this.onGround) {
-            this.vecY = 0;
+        if (this.velY > 0 && this.onGround) {
+            this.velY = 0;
         } else if (!this.onGround) {
-            this.vecY += 0.5;
+            this.velY += 0.5;
 
             // Check if player is hitting the top of the platform
             // if (this.topCheck) {
-            //     this.vecY = Math.abs(this.vecY * 0.2);
+            //     this.velY = Math.abs(this.velY * 0.2);
             // }
         }
-        this.y += this.vecY;
 
-        if (keyIsDown(LEFT_ARROW) || keyIsDown(LEFT_KEY)) {
-            if (!this.leftCheck) {
-                this.x -= 5;
-            }
-        }
-        if (keyIsDown(RIGHT_ARROW) || keyIsDown(RIGHT_KEY)) {
+        if (!this.leftCheck && (keyIsDown(LEFT_ARROW) || keyIsDown(LEFT_KEY))) {
+            this.velX = -5;
+        } 
+        else if (!this.rightCheck && (keyIsDown(RIGHT_ARROW) || keyIsDown(RIGHT_KEY))) {
             if (!this.rightCheck) {
-                this.x += 5;
+                this.velX = 5;
+            }
+        } 
+        else {
+            if (this.velX > 0) {
+                this.velX = this.rightCheck ? 0 : this.velX - 0.5;
+            } else if (this.velX < 0) {
+                this.velX = this.leftCheck ? 0 : this.velX + 0.5;
             }
         }
         if (keyIsDown(UP_ARROW) || keyIsDown(UP_KEY)) {
             // Ensure the player is on the ground (no double jumping)
             if (this.onGround) {
-                this.vecY = -10;
+                this.velY = -10;
             }
         }
+
+        // Apply velocity
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+    takeDamage(damage) {
+        this.invincibility = 40;
+        player.hp -= damage;
+        console.log("ow");
     }
     collisionCheck() {
         // Reset collision checks
@@ -65,11 +94,11 @@ class Player {
         // Platform check
         platforms.forEach(platform => {
             // Ground check
-            if (this.vecY > 0 && this.y + PLAYER_HEIGHT > platform.y && this.y + PLAYER_HEIGHT <= platform.y + platform.height && this.x + PLAYER_WIDTH >= platform.x && this.x <= platform.x + platform.width) {
+            if (this.velY > 0 && this.y + PLAYER_HEIGHT > platform.y && this.y + PLAYER_HEIGHT <= platform.y + platform.height && this.x + PLAYER_WIDTH >= platform.x && this.x <= platform.x + platform.width) {
                 this.y = platform.y - PLAYER_HEIGHT;
                 this.onGround = true;
-                if (this.vecY > 0) {
-                    this.vecY = 0;
+                if (this.velY > 0) {
+                    this.velY = 0;
                 }
             }
             // Top check
@@ -87,5 +116,45 @@ class Player {
                 this.rightCheck = true;
             }
         });
+
+        // Enemy check
+        if (this.invincibility <= 0) {
+            let damageTaken = false;
+            enemyList.forEach(enemy => {
+                if (damageTaken) {
+                    return;
+                }
+
+                // Check if the player is landing on the enemy. Knock the player up and deal damage.
+                if (this.velY > 0 && this.y + PLAYER_HEIGHT > enemy.y && this.y + PLAYER_HEIGHT <= enemy.y + enemy.height && this.x + PLAYER_WIDTH >= enemy.x && this.x <= enemy.x + enemy.width) {
+                    this.takeDamage(enemy.damage);
+                    // this.velY = -10;
+                    damageTaken = true;
+                    return;
+                }
+                // Top check
+                if (this.y >= enemy.y && this.y <= enemy.y + enemy.height && this.x + PLAYER_WIDTH >= enemy.x && this.x <= enemy.x + enemy.width) {
+                    this.takeDamage(enemy.damage);
+                    damageTaken = true;
+                    return;
+                }
+
+                // Check if the player is hitting the enemy from the left side. Knock the player back and deal damage.
+                if (this.y + PLAYER_HEIGHT > enemy.y && this.y <= enemy.y + enemy.height && this.x <= enemy.x + enemy.width && this.x >= enemy.x) {
+                    this.takeDamage(enemy.damage);
+                    // this.velX = 10;
+                    damageTaken = true;
+                    return;
+                }
+
+                // Check if the player is hitting the enemy from the right side. Knock the player back and deal damage.
+                if (this.y + PLAYER_HEIGHT > enemy.y && this.y <= enemy.y + enemy.height && this.x + PLAYER_WIDTH >= enemy.x && this.x + PLAYER_WIDTH <= enemy.x + enemy.width) {
+                    this.takeDamage(enemy.damage);
+                    // this.velX = -10;
+                    damageTaken = true;
+                    return;
+                }
+            });
+        }
     }
 }
